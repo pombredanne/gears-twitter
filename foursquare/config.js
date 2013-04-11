a0a3ae7a01de6627a4c819dccd2feb92fb423d15
Scott Plumlee <scott@plumlee.org>
2013-04-11 11:51:07 -0400

    Initial commits for the ET gears.

---
+++ foursquare/config.js
@@ -0,0 +1,22 @@
+define([], function(){
+    return {
+        "drop": {
+            "mode": "dialog",
+            "src": "edit.html"
+        },
+        "edit": {
+            "mode": "palette",
+            "src": "edit.html"
+        },
+        "icon": "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAA4xJREFUeNqkVltoFFcY/uayu91sotgY29h4F8GUBK2lQUgQqxG0QkXFQkWQtC9Kn0IJCEVEFMG+Cn0oFCV9aCAUFUFSpMlD4mUxEKpboyEqRjdR02Rz2dmZnZlz+p8zO5NsWKMbz+63Z/85e77//NezCuccP7Te2QbgDOELQhjvNzKEOKHlwpG6uHL80q1GEv4SKxFdg6YqoPeCBiO4LofluEIUH7t1xvl5wReL6HAYh+kwCKuKInZdpMdfQVEULKmoRAlxpS1bI5qTOpFVh+nktstg2u6CTq6kx9H0ZS2yjoP23heIxmIIa6rgq9EZ42HhEmEWfS+a3LYyaGrYiIbadVLuGzHx+HUa0ZAm+YQF0iWc8aJdQxtQVcICctN2MJwyPT65TAqEFk7REXOxFtjGFJoO1QXyjftJTBhZmSg8x0dBZpgBL+LwDJ+vLMOaynIpj02buNb3TPIoXAk4dZe0uEQs5yIsYMYEDu9oDOS2O09gWE4g+3zkItIU4N0UMMfGvs9Wo3xRTMr9yXH0PByeySp6+Zy673sftmVA00JQ9dAbFZRRse5vqA3k1u6BvMOJuhLuFs/U4PT0wJxKYW0pQ8T8D0ZqFPnWechm0ji6YxN0ynMxuvuTGByZKPBb7lkg/C/IxVxfXYXmr+vgUGVe7Iij/eYDxD78GKqmB2m5arGKrZ+u9tIy66C1Z0Duzc8ABJzqbK1LlyyW67qm4fs9W3Hu6C6EjNew0lNy3ZpO4dhXM2nZHh/E6GSmoKU+VM8Urw46/hlCYmgsINi8vgq/Nn+Dmo8imHw5hJ3VlVj/SYVce5lK48rdp5gbw7nwsoh72qYzDn5qu40Ddevwbf0G6qoKyko+wNnv9uLe4yQ2rFgWKP+9+xGytlO4N83KItXPVx+i6f1xcwA/tvbQKY1gU83a5YiEvFgkhkbRmXiBuXsLQc2v5Bn0J8dw/Lcu/H3/eX4N0KZfbiTwpn0B4M2q6BlguWY3B4Zp4+ervTh3+S6mMlmp4E8K7OBwquDvZ8Pn1FmuTfBcuhYaXYnnEssWRfFqMvPWSlcpBoKT5bpplu6CsOiAlj1/qxiZFZP5hqYrlADe/aLSyf81szZ0UhANa3LWlIXB5whRlRtUhMTdJ1zUQpV3fTxtadGwjjDdRAu+9Lm49BlSlrh05KV/WhG+336qXfxdOU/YQih9z78tE4RewonOUwfj/wswAMY15WyChc6HAAAAAElFTkSuQmCC",
+        "key": "foursquare",
+        "lang": {
+            "en-US": {
+                "_description": "This Gear will display venues near a specified location.",
+                "_name": "Foursquare Venues"
+            }
+        },
+        "version": "0.0.1",
+        "viewTypes": ["*"]
+    };
+});
