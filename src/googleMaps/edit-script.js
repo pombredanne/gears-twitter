17c276d963730a23d7328299451ddadabeaf88c2
Scott Plumlee <scott@plumlee.org>
2013-04-30 13:43:03 -0400

    New googleMaps gears.

--- src/googleMaps/edit-script.js
+++ src/googleMaps/edit-script.js
@@ -1,16 +1,62 @@
 /*global $, FUELUX_EDITOR */
 $(document).ready(function(){
+	// store for later
 	var view;
-	function getWebView(gearData, urlAddress) {
-		return '<iframe width="'+ gearData.width +'" height="'+ gearData.height +'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+ urlAddress +'&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q='+ urlAddress +'" target="_blank" style="color:#0000FF;text-align:left">View Larger Map</a></small>';
+
+	var origDetails;
+
+	// interactive map
+	function getWebView(gearData) {
+		return '<iframe width="'+ gearData.width +'" height="'+ gearData.height +'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+ gearData.urlAddress +'&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q='+ gearData.urlAddress +'" target="_blank" style="color:#0000FF;text-align:left">View Larger Map</a></small>';
 	}
 
-	function getHtmlView(gearData, urlAddress) {
-		return '<a href="https://maps.google.com/maps?q='+ urlAddress +'" target="_blank"><img src="http://maps.googleapis.com/maps/api/staticmap?center=' + urlAddress + '&markers=' + urlAddress + '&zoom=14&size=' + gearData.width + 'x' + gearData.height + '&sensor=false"' + ' /></a>';
+	// static map, no controls, image only, zoomable
+	function getHtmlView(gearData) {
+		return '<a href="https://maps.google.com/maps?q='+ gearData.urlAddress +'" target="_blank"><img src="http://maps.googleapis.com/maps/api/staticmap?center=' + gearData.urlAddress + '&markers=' + gearData.urlAddress + '&zoom=' + gearData.zoom + '&size=' + gearData.width + 'x' + gearData.height + '&sensor=false"' + ' /></a>';
 	}
 
-	function getTextView(gearData, urlAddress) {
-		return gearData.address + ': https://maps.google.com/maps?q=' + urlAddress;
+	function getTextView(gearData) {
+		return gearData.address + ': https://maps.google.com/maps?q=' + gearData.urlAddress;
+	}
+
+	// what content to message back to the editor
+	function getContent(view, gearData) {
+		var content;
+		if (
+			view === 'text/html; kind=viewaswebpage' || view === 'text/html; kind=htmlemailbody') {
+			content = getWebView(gearData);
+		} else if (view === 'text/html; kind=forwardtoafriend') {
+			content = getHtmlView(gearData);
+		} else if (view === 'text/plain; kind=textemailbody') {
+			content = getTextView(gearData);
+		} else {
+			content = '<div class="well text-error">Message type not supported.</div>';
+		}
+		return content;
+	}
+
+	function getDataForEditor(view) {
+		var address = $('#mapAddress').val(),
+			urlAddress = address.replace(/\s/g, "+"),
+			width = $('#mapWidth').spinner('value'),
+			height = $('#mapHeight').spinner('value'),
+			zoom = $('#mapZoom').spinner('value'),
+			content = '',
+			gearData = {
+				address: address,
+				urlAddress: urlAddress,
+				width: width,
+				height: height,
+				zoom: zoom
+			};
+		if ((address === '') || (width === '') || (height === '') || (zoom === '')) {
+			// validation here instead of allowing messaging to continue
+			content = '<div class="well text-error">Please verify that you have supplied an address, width, height, and/or zoom.</div>';
+			return false;
+		} else {
+			content = getContent(view, gearData);
+		}
+		return { content: content, gearData: gearData };
 	}
 
 	FUELUX_EDITOR.trigger('fetch');
@@ -18,19 +64,19 @@ $(document).ready(function(){
 	FUELUX_EDITOR.trigger('resize', { height: 258, width: 310 });
 
 	FUELUX_EDITOR.on('receive', function(e, details){
-		var address = '',
-			gearData = details.gearData;
+		var gearData = details.gearData;
+		origDetails = details;
 
 		view = (details && details.viewType && details.viewType.value ? details.viewType.value : null);
 
-		$('#mapWidth').spinner('value', gearData.width);
-		$('#mapHeight').spinner('value', gearData.height);
-		$('#mapZoom').spinner('value', gearData.zoom);
-
-		if (gearData.address !== undefined){
-			address = gearData.address.replace(/\+/g," ");
+		if (gearData.address && gearData.width && gearData.height && gearData.zoom) {
+			$('#mapWidth').spinner('value', gearData.width);
+			$('#mapHeight').spinner('value', gearData.height);
+			$('#mapZoom').spinner('value', gearData.zoom);
+			$('#mapAddress').val(gearData.address);
+		} else {
+			// geolocation here?
 		}
-		$('#mapAddress').val(address);
 
 		FUELUX_EDITOR.trigger('ready');
 	});
@@ -42,58 +88,40 @@ $(document).ready(function(){
 	FUELUX_EDITOR.on('accept', function(e, details){
 		// view can be stored from receive event or read now
 		view = ( details && details.viewType && details.viewType.value ? details.viewType.value : view);
-
-		var address = $('#mapAddress').val(),
-			urlAddress = address.replace(/\s/g, "+"),
-			width = $('#mapWidth').spinner('value'),
-			height = $('#mapHeight').spinner('value'),
-			zoom = $('#mapZoom').spinner('value'),
-			content = '',
-			gearData = {
-				address: address,
-				width: width,
-				height: height,
-				zoom: zoom
-			};
-
-		if ((address === '') || (width === '') || (height === '') || (zoom === '')) {
-
-			content = '<div class="well text-error">Please verify that you have supplied an address, width, height, and/or zoom.</div>';
-
-			FUELUX_EDITOR.trigger('done', { content: content, gearData: gearData });
-
-		} else {
-
-			if (
-				view === 'text/html; kind=viewaswebpage' || view === 'text/html; kind=htmlemailbody') {
-				content = getHtmlView(gearData, urlAddress);
-			} else if (view === 'text/html; kind=forwardtoafriend') {
-				content = getHtmlView(gearData, urlAddress);
-			} else if (view === 'text/plain; kind=textemailbody') {
-				content = getTextView(gearData, urlAddress);
-			} else {
-				content = '<div class="well text-error">Message type not supported.</div>';
-			}
-
-
-			FUELUX_EDITOR.trigger('done', { content: content, gearData: gearData });
-		}
+		FUELUX_EDITOR.trigger('done', getDataForEditor(view));
 	});
 
+	// click handlers for accept/cancel buttons
+	$('.editor-dialog-cancel').on('click', function(e) {
+		e.preventDefault();
+		FUELUX_EDITOR.trigger('done', { content: origDetails.content, gearData: origDetails.gearData });
+	});
+
+	$('.editor-dialog-accept').on('click', function(e) {
+		e.preventDefault();
+		FUELUX_EDITOR.trigger('done', getDataForEditor(view));
+	});
+
+	// create spinners on page
 	$('#mapWidth').spinner({
 		'value': 267,
 		'min': 200,
-		'speed': 'fast'
+		'speed': 'fast',
+		'step': 10
 	});
 	$('#mapHeight').spinner({
 		'value': 200,
 		'min': 200,
-		'speed': 'fast'
+		'speed': 'fast',
+		'step': 10
 	});
+
 	$('#mapZoom').spinner({
 		'value': 12,
 		'min': 0,
 		'max': 21,
-		'speed': 'slow'
+		'speed': 'slow',
+		'step': 1
 	});
+
 });
