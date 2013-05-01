1ce33812402c3bead1a581d734bdb2a13e1d86e7
Scott Plumlee <scott@plumlee.org>
2013-05-01 12:35:42 -0400

    Remove fuelux-imhtheme and fuelux-gearapi from the repo as front end dependencies. Call via cdn from now on.

--- lib/fuelux-gearapi/util/update-libraries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# set the current directory to this file's parent, then move up to fuelux-editor
-cd "$(dirname "$0")"
-cd ../
-
-# use volo to force add all base libraries (order is important)
-volo add jrburke/requirejs -f
-volo add kevinparkerson/postmonger -f
