348a09f8010e8eee702a036c71b697f2e6fee8c9
Scott Plumlee <scott@plumlee.org>
2013-07-11 10:53:19 -0400

    Update rss gear with changes and build.

--- src/rssFeed/config.js
+++ src/rssFeed/config.js
@@ -1,9 +1,9 @@
 define([], function(){
 	return {
 		"drop": {
-			"mode": "palette",
+			"mode": "stealth",
 			"autoUpdate": "false",
-			"src": "edit.html"
+			"src": "drop.html"
 		},
 		"edit": {
 			"mode": "palette",
@@ -11,7 +11,7 @@ define([], function(){
 			"src": "edit.html"
 		},
 		"icon": "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAABExJREFUeNqsVltMHGUU/nZmdvbCsqwUylaWVG7akKXU1tpIY0lNaYI1xrQmNaLGl74YtQ8meHkwa6MvjS8m1cTEB40P+uClMTVNUNvGpCWFgi2XFrFAobQLwpZdYHdndndmPGeWHXYbaip4wtn5//nPfOc/d2yGYeD1ry+2APiQ+HFiGeujJHE3cceJl3d12177qquVNp184pBEiIIN9Lcm0ok1zYCa0XjLP22SbhjHGa/IISGjG1AyOtiqtZCNgCRBgJuw4mpaJJj3JQJrkOnmaU2HktawXkqTHU67CFkUGK9R0nVDZpewWbTG/0EqXdRFShiPLTBdYtAmPj+LRCxiCtmdbkh2B2S3x1z/F2IPG+aTFLAWg6LDz4NPbMGh5i2m0OB4GGPhCC5em8C1m6Nw+8rgKPLeXywYnPBMC3RCz3F+cIPVm0x+tjmIv+cX8c2ZPvx+dQKeDX4Ikv3fFRg2C1PQSItGwPwUHa5VP9j4QDGOHmpBqP0pSMkIVEUx5e+Hbe2f/mZ4nHYoqQxSqTS0TAreYg8aAhuwo7oMj1WXm2mXo3hSxTtfnMKsRjGSHateiGvJLUtYVNIxMdj2SshOKZWm/C+CigqnhvDt2/hzfAo912dwbmQOsl1Cvb/E/JjXe7bW4vwfQ4hrpNgmZIOaxxwDiTApm1Tb4U86sxZQaj3TWIEX9gRNoBny+y+XhnHy/CAMlw9N9VV460ATFWTW/0tkyZuf/YSE3be6BSS3mEzFBPa/bmTjwLfJUQX5/aXWnfjy7RcRrHCid/gGQt9fQlxJm+celwNHn2uGkljMxvAuzmEKuk7RXua5pIahqTsFt2GgD15tw+4aH0Ymp3Gic9A6a6qtxLbKYuRj3M3iI63tIVnMtorR6RjODN3Cqb4J3LqzhGBVKbiNMO0O1qBnYAQD4TjKS1yo2ZiNSaDMi9N942S8WBADe7ZVqFkLjBWNylIM4ckx/Nw1gCOfn8XYTMy6ccfhvVQ4Kr698Jf1rq6yHAGv/Z4WCPk5qyYTeLKuFB8feRoHmgKYi0Tw0Y+9WFJSJpi/1IudD5UiPJ/AheEpS8muev8960DIr+SGB4vxXvt+PFofwBsHW7CvsQrh6BJO9oxaYPu315myXSNh693Waj/ycUzGciVzz4CebXbbH95cEOBNZT7z/a/9K7dl5Tp13rHZ+EoiOGVTLp9zmIK+3Ca4D3X23yR3pK0P+6eiZrqFo/GC91zZ16ej1r4+UG7K5Zg7GmPy2rbv2A+qU5bMmZBQM6jwueEvcWM6lsBMNGGB1FIle5aLbJQCzwqbNpdZ51cm5qy1i9oEZ1IilYnxPLiqpNLbvC6ZDkREFpKYiyWzFWlbGc43ZhYKq5XOBicjBXsWt0uCmaILyRR75TLP5A6y6fR8XBVZs0yTaM1D3+ChryOqKlwPPH+P2dj3e0Pf8b8rx4l3cMzWOTG5cHqJ3z0ber77HwEGALqer87J6pU8AAAAAElFTkSuQmCC",
-		"key": "blogposts",
+		"key": "rssFeed",
 		"lang": {
 			"en-US": {
 				"_description": "This Gear will create an embedded RSS feed.",
---
+++ src/rssFeed/drop-script.js
@@ -0,0 +1,25 @@
+/*global $, FUELUX_EDITOR */
+var defaults = {
+  feedurl: 'http://blog.exacttarget.com/blog/the-exacttarget-blog/rss',
+  postcount: 5
+};
+
+$(document).ready(function(){
+
+  $.getJSON(document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num='+ defaults.postcount +'&callback=?&q=' + encodeURIComponent(defaults.feedurl),{},function(jsonData){
+
+    var entries = jsonData.responseData.feed.entries;
+    var content = '';
+
+    _.each(entries, function(entry) {
+      content += '<div>' + entry.title + '</div>';
+    });
+
+    FUELUX_EDITOR.trigger('done', {
+      content: 'default content',
+      design: content,
+      gearData: defaults
+    });
+  });
+
+});
