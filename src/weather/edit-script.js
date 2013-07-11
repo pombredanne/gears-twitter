2aa8718ecd6af108a922644703da46d20f6a8bd6
Scott Plumlee <scott@plumlee.org>
2013-07-11 10:30:54 -0400

    Add weather gear.

---
+++ src/weather/edit-script.js
@@ -0,0 +1,48 @@
+/*global $, FUELUX_EDITOR */
+$(document).ready(function() {
+	var zipcode = '';
+
+	$('#searchField').on('searched', function(event, searchText) {
+		zipcode = searchText;
+
+		$.ajax({
+			url : 'http://api.wunderground.com/api/a9098e8c1b9fb593/geolookup/conditions/q/IA/' + zipcode + '.json',
+			dataType : 'jsonp',
+			success : function(data) {
+				var location = data.location.city + ', ' + data.location.state;
+				var temp = data.current_observation.temp_f;
+
+				$('#output').html('current temperature in ' + location + ' is: ' + temp + '&deg;<br/>' + data.current_observation.weather + ' <img src="' + data.current_observation.icon_url + '" />');
+			}
+		});
+	});
+
+	$('#searchField').on('cleared', function() {
+		$('#output').html('');
+	});
+
+	FUELUX_EDITOR.trigger('resize', { height:200, width:200 });
+	FUELUX_EDITOR.trigger('fetch');
+
+	// load gear, fires on response to the 'fetch' event
+	FUELUX_EDITOR.on('receive', function(e, details) {
+		console.log('receive', details);
+
+		FUELUX_EDITOR.trigger('ready');
+	});
+
+	// close and populate editor with gear data
+	FUELUX_EDITOR.on('accept', function(e, details) {
+
+		var html = $('#output').html();
+		var json = {
+			zip: zipcode
+		};
+
+		FUELUX_EDITOR.trigger('done', {
+			content: html,
+			design: html,
+			gearData: json
+		});
+	});
+});
